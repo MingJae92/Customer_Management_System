a3e9868b45d00ab0e95daf0efa79566b00030373
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-12-24 01:07:28 +0000

    Form implemented

--- src/components/Customerform.js
+++ src/components/Customerform.js
@@ -1,29 +1,58 @@
-import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import axios from "axios"
 
-function Customerform() {
+function CustomerForm() {
   const [form, setForm] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    address: ''
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
   });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitForm = async(e) => {
+    e.preventDefault();
+    const customerData = {
+      name:form.name,
+      email:form.email,
+      phone:form.phone,
+      address:form.address
+    }
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_URL}`, customerData)
+      console.log(response.data)
+    } catch (error) {
+      console.log("Error submitting form", error)
+    }
+   
+  };
+
   return (
     <Container>
-    
-    <Row className="justify-content-md-center mb-4">
-      <Col xs={12} md={2} className="border p-4">
-        <Form>
+      <Col xs="auto">
+        <h1 className="text-center">Customer Management System</h1>
+      </Col>
+      <Col xs="auto">
+        <Form onSubmit={submitForm} style={{ border: "1px solid #ccc", padding: "20px", borderRadius: "8px" }} className="mx-auto">
           <Form.Group className="mb-3" controlId="formBasicName">
             <Form.Label>Name</Form.Label>
             <Form.Control
               type="text"
               placeholder="Enter name"
               name="name"
+              value={form.name}
+              onChange={handleChange}
             />
           </Form.Group>
 
@@ -33,6 +62,8 @@ function Customerform() {
               type="email"
               placeholder="Enter email"
               name="email"
+              value={form.email}
+              onChange={handleChange}
             />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
@@ -45,6 +76,8 @@ function Customerform() {
               type="text"
               placeholder="Enter phone"
               name="phone"
+              value={form.phone}
+              onChange={handleChange}
             />
           </Form.Group>
 
@@ -54,6 +87,8 @@ function Customerform() {
               as="textarea"
               placeholder="Enter address"
               name="address"
+              value={form.address}
+              onChange={handleChange}
             />
           </Form.Group>
 
@@ -62,10 +97,8 @@ function Customerform() {
           </Button>
         </Form>
       </Col>
-    </Row>
-  </Container>
-
+    </Container>
   );
 }
 
-export default Customerform;
+export default CustomerForm;
