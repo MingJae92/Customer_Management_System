1f6c8494a36a2991cad24d1fc3295cf223f2141b
Ming Chi <wong_mingchi@yahoo.co.uk>
2023-12-31 00:56:32 +0000

    middle ware route paths updated

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"; 
 import { db_connection } from "./mongo_db_connection.js";
-import CustomerRoutes from './routes/customer_route.js';
+import PostCustomerDetails from './routes/customer_route.js';
+import GetCustomerDetails from './routes/customer_route.js'
 
 // Load environment variables from the specified file
 dotenv.config({ path: '../config/.env' }); 
@@ -20,15 +21,15 @@ const port = process.env.SERVER_PORT || 5000;
 // Middleware to parse incoming JSON requests
 app.use(express.json());
 
-
-
 // Start the server and listen on the specified port
 app.listen(port, () => {
     console.log(`Server connecting to ${port}`);
 });
 // Use the customerRoutes for requests to the /customerdataapi endpoint
 // app.use("/customerdataapi", CustomerRoutes);
-app.use("/", CustomerRoutes);
+app.use("/customerdataapi", PostCustomerDetails);
+app.use("/", GetCustomerDetails);
+
 // Get the MongoDB connection URL from environment variables
 const db_url = process.env.MONGO_DB_URL;
 
